c63f9f85e242c23e251363ced7a9961a3d8c7c11
abc <abc@abcs-MacBook-Pro.local>
2021-08-01 01:21:52 +0530

    pouch couch integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const electron = window.require('electron');
+const { ipcRenderer } = electron;
+// access the local file system; HOWEVER, `remote.require` is considered harmful
+// https://medium.com/@16cards/remote-require-is-considered-harmful-and-should-be-avoided-in-general-8282567a851
+// const fs = electron.remote.require('fs');
+
+class App extends Component {
+  componentDidMount() {
+    // Electron IPC example
+    ipcRenderer.on('manipulatedData', function (event, arg) {
+      console.log(arg);
+    });
+  }
+  componentWillUnmount() {
+    // Electron IPC example
+    ipcRenderer.removeAllListeners('manipulatedData');
+  }
+  sendIpcData = () => {
+    // Electron IPC example
+    ipcRenderer.send('user-data', 'important INFO!');
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+        <button onClick={this.sendIpcData}>Send IPC Data</button>
+      </div>
+    );
+  }
 }
 
 export default App;
